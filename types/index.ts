958e9e58a5db7d7969fbecb4bcfc570874e3d369
Hoso <hossam.alasklany@gmail.com>
2023-07-21 14:24:24 +0300

    modified

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,9 @@ export interface CustomBtnProps {
     containerStyles: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
+    textStyles: string;
+    rightIcon: string;
+    isDisabled: boolean;
 
 }
 
@@ -15,4 +18,27 @@ export interface CustomFilterProps {
 export interface SearchManuFacturerProps {
     manuFacturer: string;
     setManuFacturer: (manufacturer: string) => void;
+}
+
+export interface CarsProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
+export interface FilterProps {
+    manuFacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
 }
