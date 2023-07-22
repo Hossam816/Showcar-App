20d2691c21f29a428f0e7c251061abca49d6b1c5
Hoso <hossam.alasklany@gmail.com>
2023-07-22 18:44:50 +0300

    modified

--- utils/index.ts
+++ utils/index.ts
@@ -44,3 +44,16 @@ export const generateCarImg = (car: CarsProps, angle?: string) => {
 
     return `${url}`
 }
+
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+
+    const newPathName = `${window.location.pathname
+        }? ${searchParams.toString()}`;
+
+    return newPathName;
+}
